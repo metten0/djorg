734de28740855a4133ebf6a9e89fb8a0e4a9298b
Jason Metten <metten@Jasons-MacBook-Air.local>
2018-04-11 17:25:17 -0500

    MVP runs

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from .models import Bookmark
-# Create your views here.
 
 
 def index(request):
-  context = {'bookmarks': Bookmark.objects.all()}
-  return render (request, 'bookmarks/index.html', context)
+    context = {'bookmarks': Bookmark.objects.all()}
+    return render(request, 'bookmarks/index.html', context)
